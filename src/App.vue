e7932c8d4a6386841827c8767a04efd6673c7000
DegasperisRomain <rdegasperis11@gmail.com>
2024-06-09 16:45:22 +0200

    add responsive

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,14 @@
       <MainTitle />
       <FooterSection />
     </div>
-    <img src="./assets/images/image-home.png" alt="Sketch d'une maison d'architecte">
+    <img src="./assets/images/image-home.png" class="img-section" alt="Sketch d'une maison d'architecte">
   </section>
 
   <section class="second-section">
-    <img src="./assets/images/image-section2.png" alt="Sketch d'une maison d'architecte">
+    <img src="./assets/images/image-section2.png" class="img-section" alt="Sketch d'une maison d'architecte">
 
     <div class="right">
-      <TitleSection
-      :title="'Notre Histoire'"
-      :subTitle="'1984'"
-      >
+      <TitleSection :title="'Notre Histoire'" :subTitle="'1984'">
 
       </TitleSection>
       <GridContent />
@@ -23,11 +20,7 @@
   </section>
 
   <section class="section-three">
-    <TitleSection
-      :title="'Service'"
-      :subTitle="'our'"
-      style="text-align: left"
-      ></TitleSection>
+    <TitleSection :title="'Service'" :subTitle="'our'" style="text-align: left"></TitleSection>
     <ListService />
   </section>
 
@@ -96,5 +89,203 @@ footer {
   justify-content: space-between;
   background: #060606;
   padding: 0 24px;
+}
+
+
+@media screen and (max-width: 1024px) {
+
+  #app .img-section {
+    width: 50vw;
+  }
+
+  h1.title-section {
+    width: 50%;
+  }
+
+  .section-three {
+    margin: 100px;
+  }
+
+  footer img {
+    width: 60%;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .first-section {
+    flex-direction: column;
+  }
+
+  #app .first-section .left {
+    width: 100vw;
+    height: 100vh;
+
+  }
+
+  #app .first-section .img-section {
+    width: 100vw;
+  }
+
+  #app .second-section .right {
+    width: 100vw;
+
+    .parent {
+
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+
+    }
+
+    .div1 {
+      grid-area: 1 / 1 / 2 / 2;
+      margin-top: -70px;
+    }
+
+    .div2 {
+      grid-area: 1 / 2 / 2 / 3;
+    }
+
+    .div3 {
+      grid-area: 2 / 1 / 3 / 2;
+    }
+  }
+
+  #app .second-section .img-section {
+    width: 100vw;
+  }
+
+  .second-section {
+    flex-direction: column-reverse;
+
+
+    .parent {
+      grid-template-columns: 1fr;
+      grid-template-rows: repeat(3, 1fr);
+      grid-row-gap: 20px;
+
+      .div1 {
+        grid-area: 2 / 1 / 3 / 2;
+        margin-top: 10px
+      }
+
+      .div2 {
+        grid-area: 1 / 1 / 2 / 2;
+      }
+
+      .div3 {
+        grid-area: 3 / 1 / 4 / 2;
+      }
+    }
+  }
+
+  .section-three {
+    .container {
+      flex-direction: column;
+      gap: 40px;
+
+      .block {
+        width: 100%;
+      }
+
+
+      p {
+        text-align: justify;
+      }
+
+      h6 {
+        text-align: justify;
+        width: 100%;
+      }
+    }
+  }
+
+  .galery {
+    .parent {
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(5, 1fr);
+      padding: 0 100px;
+
+      .div1 {
+        grid-area: 1 / 1 / 2 / 2;
+      }
+
+      .div2 {
+        grid-area: 1 / 2 / 2 / 3;
+      }
+
+      .div3 {
+        grid-area: 4 / 1 / 5 / 2;
+      }
+
+      .div4 {
+        grid-area: 3 / 2 / 5 / 3;
+      }
+
+      .div5 {
+        grid-area: 2 / 1 / 3 / 3;
+      }
+
+      .div6 {
+        grid-area: 3 / 1 / 4 / 2;
+      }
+
+      .div7 {
+        grid-area: 5 / 1 / 6 / 2;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 425px) {
+  #app .first-section {
+    .left {
+      height: 100vh;
+    }
+  }
+
+
+  #app h1 {
+    width: 300px;
+    font-size: 42px;
+  }
+
+  #app .second-section .right {
+    width: 100vw;
+
+    .parent {
+      display: flex;
+      flex-direction: column;
+      gap: 40px;
+
+      div {
+        text-align: justify;
+      }
+    }
+  }
+
+  .galery {
+    .parent {
+      padding: 0 20px;
+    }
+  }
+
+  footer {
+    flex-direction: column-reverse;
+    gap: 40px;
+
+    img {
+      width: 100%;
+    }
+
+    .content {
+      align-items: center;
+      height: 50vh;
+
+
+      ul {
+        padding-left: 0;
+      }
+    }
+  }
 }
 </style>
--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -1,39 +1,39 @@
 <template>
-    <h1>
+    <h1 class="title-section">
         <span>{{ subTitle }}</span>
         {{ title }}
     </h1>
 </template>
 
 <script setup>
-    defineProps({
-        title: String,
-        subTitle: String,
-    })
+defineProps({
+    title: String,
+    subTitle: String,
+})
 
 </script>
 
 <style lang="scss" scoped>
-    h1 {
-        color: #FFF;
-        text-align: right;
-        font-family: 'Avenir';
-        font-size: 67px;
-        font-style: normal;
-        font-weight: 800;
-        text-transform: uppercase;
-        line-height: normal;
-        display: flex;
-        flex-direction: column;
-        width: max-content;
+h1 {
+    color: #FFF;
+    text-align: right;
+    font-family: 'Avenir';
+    font-size: 67px;
+    font-style: normal;
+    font-weight: 800;
+    text-transform: uppercase;
+    line-height: normal;
+    display: flex;
+    flex-direction: column;
+    width: max-content;
 
-        span {
-            color: rgba(255, 255, 255, 0.30);
-            font-family: 'Neue Montreal', sans-serif;
-            font-size: 20px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: normal;
-        }
+    span {
+        color: rgba(255, 255, 255, 0.30);
+        font-family: 'Neue Montreal', sans-serif;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
     }
+}
 </style>
